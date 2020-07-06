60d00007056273a12bf3b011175f113d7206788e
elumixor <vladogim97@gmaill.com>
2020-07-07 00:21:51 +0200

    Finished refactoring, ready for first production release!

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -9,8 +9,8 @@ namespace Player {
             if (Input.touchCount > 0) {
                 var touch = Input.GetTouch(0);
                 if (touch.phase == TouchPhase.Began) {
-                    if (Input.GetTouch(0).position.x >= Screen.width / 2f) PlayerController.RotateRight();
-                    else PlayerController.RotateLeft();
+                    if (Input.GetTouch(0).position.x >= Screen.width / 2f) PlayerController.RotateLeft();
+                    else PlayerController.RotateRight();
                 }
             }
         }
--- Assets/Scripts/ScoreLabel.cs
+++ Assets/Scripts/ScoreLabel.cs
@@ -4,20 +4,18 @@ using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
 
+/**
+ * Score label displays current score in white color. When it displays highscore, it starts showing stars.
+ * Displays highscore on startup.
+ * If highscore is zero, does not display it.
+ */
 public class ScoreLabel : SingletonBehaviour<ScoreLabel> {
     [SerializeField] private TMPro.TextMeshProUGUI label;
+    [SerializeField] private List<DisplayedAnimationToggle> stars = new List<DisplayedAnimationToggle>();
 
     private static int highscore;
     private static int score;
     private static bool displayHighscore = true;
-
-    public static bool DisplayHighscore {
-        set {
-            instance.label.text = value ? highscore.ToString() : score.ToString();
-
-            displayHighscore = value;
-        }
-    }
 
     public static int Score {
         get => score;
@@ -28,19 +26,27 @@ public class ScoreLabel : SingletonBehaviour<ScoreLabel> {
                 PlayerPrefs.SetInt("highscore", score);
             }
 
-            if (!displayHighscore) {
-                instance.label.text = score.ToString();
-            }
+            instance.Display(score);
         }
-    }
-
-    public Color Color {
-        set => label.faceColor = value;
     }
 
     protected override void Awake() {
         base.Awake();
-        highscore = PlayerPrefs.GetInt("highscore", 0);
-        label.text = highscore.ToString();
+        score = highscore = PlayerPrefs.GetInt("highscore", 0);
+        Display(highscore);
+    }
+
+    private void Display(int score) {
+        if (score == 0) {
+            label.text = "";
+            foreach (var star in stars) star.Displayed = false;
+            return;
+        }
+
+        label.text = score.ToString();
+
+        var isHighscore = score == highscore;
+
+        foreach (var star in stars) star.Displayed = isHighscore;
     }
 }
